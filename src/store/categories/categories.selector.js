2f8b8055067b3ecba949943034a55c02c70d5f97
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-13 23:47:38 +0900

    #162 Reselect library 2

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,11 +1,19 @@
+import {createSelector} from "reselect";
+
+const selectCategoryReducer = (state) => state.categories
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+
 export const selectCategoriesMap = (state) => {
     console.log("selector fired")
-    const categoriesMap = state.categories.categories.reduce(
+    return state.categories.categories.reduce(
         (acc, {title, items}) => {
             acc[title.toLowerCase()] = items;
             return acc;
         },
         {}
     );
-    return categoriesMap;
 };
