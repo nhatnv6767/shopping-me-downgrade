8f4a52416077290d159b3f4642e913f40d244d21
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-15 00:33:51 +0900

    #163 Migrating Cart Context to redux <3> 19

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -6,14 +6,16 @@ import {
     Name,
     Price,
 } from './product-card.styles';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {addItemToCart} from "../../store/cart/cart.action";
+import {selectCartItems} from "../../store/cart/cart.selector";
 
 const ProductCard = ({product}) => {
     const {name, price, imageUrl} = product;
+    const cartItems = useSelector(selectCartItems)
     const dispatch = useDispatch()
 
-    const addProductToCart = () => dispatch(addItemToCart(product))
+    const addProductToCart = () => dispatch(addItemToCart(cartItems, product))
 
     return (
         <ProductCartContainer>
