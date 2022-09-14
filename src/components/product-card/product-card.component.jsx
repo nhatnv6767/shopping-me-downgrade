a381822ea57af09e3c204dd4a38d6c1d122bfdce
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-15 00:31:56 +0900

    #163 Migrating Cart Context to redux <3> 18

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,37 +1,35 @@
-import { useContext } from 'react';
-
-import { CartContext } from '../../contexts/cart.context';
-
-import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 
 import {
-  ProductCartContainer,
-  Footer,
-  Name,
-  Price,
+    ProductCartContainer,
+    Footer,
+    Name,
+    Price,
 } from './product-card.styles';
+import {useDispatch} from "react-redux";
+import {addItemToCart} from "../../store/cart/cart.action";
 
-const ProductCard = ({ product }) => {
-  const { name, price, imageUrl } = product;
-  const { addItemToCart } = useContext(CartContext);
+const ProductCard = ({product}) => {
+    const {name, price, imageUrl} = product;
+    const dispatch = useDispatch()
 
-  const addProductToCart = () => addItemToCart(product);
+    const addProductToCart = () => dispatch(addItemToCart(product))
 
-  return (
-    <ProductCartContainer>
-      <img src={imageUrl} alt={`${name}`} />
-      <Footer>
-        <Name>{name}</Name>
-        <Price>{price}</Price>
-      </Footer>
-      <Button
-        buttonType={BUTTON_TYPE_CLASSES.inverted}
-        onClick={addProductToCart}
-      >
-        Add to card
-      </Button>
-    </ProductCartContainer>
-  );
+    return (
+        <ProductCartContainer>
+            <img src={imageUrl} alt={`${name}`}/>
+            <Footer>
+                <Name>{name}</Name>
+                <Price>{price}</Price>
+            </Footer>
+            <Button
+                buttonType={BUTTON_TYPE_CLASSES.inverted}
+                onClick={addProductToCart}
+            >
+                Add to card
+            </Button>
+        </ProductCartContainer>
+    );
 };
 
 export default ProductCard;
