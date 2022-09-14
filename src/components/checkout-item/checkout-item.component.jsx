fd4a4b4d34280be0976a55db1b21c07eadad6056
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-15 00:46:42 +0900

    #163 Migrating Cart Context to redux <4> 22

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -7,16 +7,18 @@ import {
     Value,
     RemoveButton,
 } from './checkout-item.styles';
+import {addItemToCart, clearItemFromCart, removeItemFromCart} from "../../store/cart/cart.action";
+import {useDispatch, useSelector} from "react-redux";
+import {selectCartItems} from "../../store/cart/cart.selector";
 
 const CheckoutItem = ({cartItem}) => {
     const {name, imageUrl, price, quantity} = cartItem;
+    const cartItems = useSelector(selectCartItems)
+    const dispatch = useDispatch()
 
-    const {clearItemFromCart, addItemToCart, removeItemToCart} =
-        useContext(CartContext);
-
-    const clearItemHandler = () => clearItemFromCart(cartItem);
-    const addItemHandler = () => addItemToCart(cartItem);
-    const removeItemHandler = () => removeItemToCart(cartItem);
+    const clearItemHandler = () => dispatch(clearItemFromCart(cartItem))
+    const addItemHandler = () => dispatch(addItemToCart(cartItem))
+    const removeItemHandler = () => dispatch(removeItemFromCart(cartItem))
 
     return (
         <CheckoutItemContainer>
