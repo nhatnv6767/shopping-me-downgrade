69f84d6944de21ccd1ed5a406014a14d5dc5db07
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-16 21:26:57 +0900

    #168 Redux - Dev tools 4

--- src/store/store.js
+++ src/store/store.js
@@ -15,6 +15,8 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean)
 
+const composeEnhancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
