043b15be3269223389d98b7c68e374c280a6f0cc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-17 08:05:44 +0900

    #169 Asynchronous redux: redux-thunk 8

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -17,6 +17,8 @@ export const categoriesReducer = (
             return {...state, isLoading: true}
         case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
             return {...state, categories: payload, isLoading: false};
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+            return {...state, error: payload, isLoading: false};
         default:
             return state;
     }
