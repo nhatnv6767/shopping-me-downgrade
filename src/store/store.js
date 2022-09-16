351f645b2662486e618c439f635fabfab9dd37da
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-17 07:58:09 +0900

    #169 Asynchronous redux: redux-thunk 2

--- src/store/store.js
+++ src/store/store.js
@@ -15,6 +15,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean)
 
+const thunkMiddleware = (store) => (next) => (action) => {
+    if (typeof (action) === 'function') {
+        action();
+    }
+}
+
 const composeEnhancer = (
     process.env.NODE_ENV
     !== "production"
