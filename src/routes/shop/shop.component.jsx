2b0a4cc4562af6ae53d537c446215b1dac6adbd9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-18 01:21:53 +0900

    #174 Redux saga: fetchCategoriesAsync Thunk to Saga 10

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,24 @@
-import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { fetchCategoriesAsync } from '../../store/categories/categories.action';
+import {useEffect} from 'react';
+import {Routes, Route} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import {fetchCategoriesStart} from '../../store/categories/categories.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
 const Shop = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchCategoriesAsync());
-  }, []);
+    useEffect(() => {
+        dispatch(fetchCategoriesStart());
+    }, []);
 
-  return (
-    <Routes>
-      <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route index element={<CategoriesPreview/>}/>
+            <Route path=':category' element={<Category/>}/>
+        </Routes>
+    );
 };
 
 export default Shop;
