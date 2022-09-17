300581379dbd2ff175dba66459a3e0b9fe4ec137
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-18 00:50:23 +0900

    #174 Redux saga: fetchCategoriesAsync Thunk to Saga 1

---
+++ src/store/categories/categories.saga.js
@@ -0,0 +1,13 @@
+import {getCategoriesAndDocuments} from "../../utils/firebase/firebase.utils";
+import {fetchCategoriesFailed, fetchCategoriesStart, fetchCategoriesSuccess} from "./categories.action";
+import {takeLatest, all, call, put} from "redux-saga/effects"
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart())
+    try {
+        const categoriesArray = await getCategoriesAndDocuments('categories');
+        dispatch(fetchCategoriesSuccess(categoriesArray))
+    } catch (error) {
+        dispatch(fetchCategoriesFailed(error))
+    }
+}
