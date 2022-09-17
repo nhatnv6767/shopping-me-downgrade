07e588bff8941754423b0b3ab7f1fbb60211819e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-17 18:17:57 +0900

    #170 Redux-thunk 2

--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -4,3 +4,9 @@ import { createAction } from '../../utils/reducer/reducer.utils';
 export const setCategories = (categoriesArray) =>
   createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
 
+export const fetchCategoriesStart = () =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+export const fetchCategoriesSuccess = (categoriesArray) =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray)
+export const fetchCategoriesFailed = () =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED)
