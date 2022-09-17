8d6d5abb26e5dd88ba9da8e3d0f3a8c1d8f7f29b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-17 17:46:29 +0900

    #170 Redux-thunk 1

--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -1,5 +1,6 @@
 import CATEGORIES_ACTION_TYPES from './categories.types';
 import { createAction } from '../../utils/reducer/reducer.utils';
 
-export const setCategories = (categories) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
+export const setCategories = (categoriesArray) =>
+  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+
