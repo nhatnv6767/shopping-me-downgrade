d2f3aff53aa4d3de6e4f3e663c56ade2c6c8271b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-20 21:08:58 +0900

    #176 Redux saga: Check User Session Saga 6

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,5 +1,16 @@
 import {takeLatest, put, all, call} from 'redux-saga/effects'
 import {USER_ACTION_TYPES} from "./user.types"
+import {getCurrentUser} from "../../utils/firebase/firebase.utils";
+
+export function* isUserAuthenticated() {
+    try {
+        const userAuth = yield call(getCurrentUser)
+        if (!userAuth) return;
+
+    } catch (e) {
+
+    }
+}
 
 export function* onCheckUserSession() {
     yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION,)
