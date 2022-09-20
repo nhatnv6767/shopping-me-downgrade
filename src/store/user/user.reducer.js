47e7b2dde845aeb39f47183abfc77284f1925885
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-20 21:03:56 +0900

    #176 Redux saga: Check User Session Saga 4.1

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,21 +1,21 @@
-import USER_ACTION_TYPES from './user.types';
+import {USER_ACTION_TYPES} from './user.types';
 
 export const USER_INITIAL_STATE = {
-  currentUser: null,
-  isLoading: false,
-  error: null,
+    currentUser: null,
+    isLoading: false,
+    error: null,
 };
 
 export const userReducer = (state = USER_INITIAL_STATE, action = {}) => {
-  const {type, payload} = action;
+    const {type, payload} = action;
 
-  switch (type) {
-    case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-      return {...state, currentUser: payload};
+    switch (type) {
+        case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
+            return {...state, currentUser: payload};
 
-    case USER_ACTION_TYPES.SIGN_IN_FAILED:
-      return {...state, error: payload};
-    default:
-      return state;
-  }
+        case USER_ACTION_TYPES.SIGN_IN_FAILED:
+            return {...state, error: payload};
+        default:
+            return state;
+    }
 };
