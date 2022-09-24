e8630fd7efd3d8cd1ef2d160aa2a20fe82516c9e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-24 11:31:09 +0900

    #180 Redux saga: Sign-out sagas 1.2

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -22,3 +22,8 @@ export const signupSuccess = (user, additionalDetails) => createAction(USER_ACTI
     {user, additionalDetails})
 
 export const signUpFailed = (error) => createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error)
+
+export const signOutStart = () => createAction(USER_ACTION_TYPES.SIGN_OUT_START)
+
+export const signOutSuccess = () => createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS)
+export const signOutFailed = () => createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED)
