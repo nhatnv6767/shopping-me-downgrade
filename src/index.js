5b68c74da7bd8ea219ba8fe60dc4ae1d401aa461
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-25 19:34:37 +0900

    #183 Setting up Stripe 5

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {persistor, store} from './store/store';
 import './index.scss';
 import {PersistGate} from "redux-persist/integration/react";
 import {Elements} from "@stripe/react-stripe-js";
+import {stripePromise} from "./utils/stripe/stripe.utils";
 
 const rootElement = document.getElementById('root');
 
@@ -17,7 +18,7 @@ render(
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
                 <BrowserRouter>
-                    <Elements stripe={}>
+                    <Elements stripe={stripePromise}>
                         <App/>
                     </Elements>
                 </BrowserRouter>
