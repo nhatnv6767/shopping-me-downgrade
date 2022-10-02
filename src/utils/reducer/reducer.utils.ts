9821447f2c7aecc4b04cfd885b8140ce1a5dc26b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-10-02 22:00:39 +0900

    #198 Typing createAction and function overloading 6

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -11,6 +11,8 @@ export type Action<T> = {
 
 export function createAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T, P>;
 
+export function createAction<T extends string>(type: T, payload: void): Action<T>;
+
 export function createAction<T extends string, P>(type: T, payload: P) {
     return {type, payload}
 }
