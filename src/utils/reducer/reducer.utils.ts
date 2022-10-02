21a4cd9333c095c8c710afa4a18104267152ca01
nhatnv6767 <nhatnvse90183@gmail.com>
2022-10-02 20:43:25 +0900

    #198 Typing createAction and function overloading 3

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -2,7 +2,11 @@ import {AnyAction} from "redux";
 
 export type ActionWithPayload<T, P> = {
     type: T;
-    payload: P;
+    payload?: P;
+}
+
+export type Action<T> = {
+    type: T;
 }
 
 export const createAction = (type, payload) => ({type, payload});
